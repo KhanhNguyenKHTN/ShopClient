162330c8b6f382c0966a8ecbb9f11d407c8c9765
KhanhNguyenKHTN <nguyenvankhanhxm123@gmail.com>
2023-02-05 19:48:22 +0700

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductComponent } from './components/product/product.component';
+import { SearchComponent } from './components/search/search.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children:[
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      { path: 'home', component:HomeComponent },
+      { path: 'product', component:ProductComponent },
+      { path: 'search', component:SearchComponent },
+      { path: 'cart', component:CartComponent },
+      { path: 'login', component:LoginComponent },
+      { path: 'sign-up', component:SignUpComponent }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { MiniProductComponent } from './components/mini-product/mini-product.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { SearchComponent } from './components/search/search.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    MiniProductComponent,
+    HomeComponent,
+    ProductComponent,
+    SearchComponent,
+    CartComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
